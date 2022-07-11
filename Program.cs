97c68d010ce3e7c3bd307ca117a70eb68b3f9a12
Jonathan Rupp <jrupp@rightpoint.com>
2022-07-11 11:42:23 -0500

    Add shell of test app

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .Build();
+
+var config = host.Services.GetService<IConfiguration>()!;
+
+var site = config["Site"];
+var libTitle = config["DocumentLibraryTitle"];
+var clientId = config["ClientId"];
+var clientSecret = config["ClientSecret"];
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
